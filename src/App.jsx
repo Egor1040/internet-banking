7fdeb1c871f40c34dac3f855e7ef06ebe6774efe
Dmytro <dm.kalitventsev@gmail.com>
2023-10-12 14:12:57 +0300

    fix error import ServicesMenu and ServicesWidget

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './common.scss';
 import Footer from './layout/Footer/Footer';
-import Header from './components/Header';
 import Main from './layout/Main/Main';
 
 function App() {
     return (
         <div className="wrapper">
-            <Header />
             <Main />
             <Footer />
         </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,3 @@
-export * from './UI/Buttons/WidgetButton/WidgetButton';
-export * from './UI/Forms/FastTransferCard/FastTransferCard';
-export * from './UI/Forms/FastMobileTopUp/FastMobileTopUp';
-export * from './UI/Forms/FastRequisitesPay/FastRequisitesPay';
-
 export * from './ServicesMenu/ServicesMenu/ServicesMenu';
 export * from './ServicesMenu/ServicesSubmenu/ServicesSubmenu';
 export * from './ServicesMenu/ServiceItem/ServiceItem';
@@ -10,3 +5,8 @@ export * from './ServicesMenu/ServiceItem/ServiceItem';
 export * from './ServicesWidget/ServicesWidget/ServicesWidget';
 export * from './ServicesWidget/ServiceWidget/ServiceWidget';
 export * from './ServicesWidget/BankNews/BankNews';
+
+export * from './UI/Buttons/WidgetButton/WidgetButton';
+export * from './UI/Forms/FastTransferCard/FastTransferCard';
+export * from './UI/Forms/FastMobileTopUp/FastMobileTopUp';
+export * from './UI/Forms/FastRequisitesPay/FastRequisitesPay';
