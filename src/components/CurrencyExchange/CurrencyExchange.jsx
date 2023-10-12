edd12d9eb545edd3a66c800e64af0b2176723188
Igor <igor.motriy3@gmail.com>
2023-10-12 21:48:32 +0300

    API currency exchange interface & add custom hook useFetch

--- src/components/CurrencyExchange/CurrencyExchange.jsx
+++ src/components/CurrencyExchange/CurrencyExchange.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import '../CurrencyExchange/currencyexchange.scss';
+import '../CurrencyExchange/currencyExchange.scss';
+import { useFetch } from '../../utils/hooks/useFetch';
 
 const CurrencyExchange = () => {
+    const data = useFetch('https://api.monobank.ua/bank/currency');
 
+    if (!data.res || data.res.length === 0) {
+        return <div>Завантаження...</div>;
+    }
 
     return (
         <div className="header-exchange">
             <div className="header-exchange__icon">
-                <img src="img/icons/dollar.svg" alt="" />
+                <img src="img/icons/dollar.svg" alt="USD-icon" />
             </div> 
-            36.5 /37.4532
+            {data.res[0].rateBuy}   /{data.res[0].rateSell}
         </div>
     );
 };
